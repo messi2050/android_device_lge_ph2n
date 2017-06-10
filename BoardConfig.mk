dc9804948c012227f3932c8d9c65eed89510669e
messi2050 <minoforever216@gmail.com>
2017-06-11 00:56:33 +0200

    ph2n: Enable TARGET_USES_MEDIA_EXTENSIONS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,9 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Media Extensions
+TARGET_USES_MEDIA_EXTENSIONS := true
+
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 

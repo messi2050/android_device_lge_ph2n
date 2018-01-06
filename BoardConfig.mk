094f49d20fdcea7ba714a3de43b3a236f9a589cd
messi2050 <minoforever216@gmail.com>
2018-01-06 04:30:09 +0200

    ph2n: update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 
+# Properties
+TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
+
 # inherit from the proprietary version
 -include vendor/lge/ph2n/BoardConfigVendor.mk
 -include device/lge/common/BoardConfigCommon.mk
--- lineage.mk
+++ lineage.mk
@@ -15,6 +15,7 @@
 #
 
 # Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from ph2n device

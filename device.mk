a7ecea9e66908af8fcaae85871448cd34e53052f
messi2050 <minoforever216@gmail.com>
2018-03-15 16:08:13 +0200

    ph2n: fingerprint  fix

--- device.mk
+++ device.mk
@@ -42,7 +42,15 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 #PRODUCT_PACKAGES += \
-#    android.hardware.biometrics.fingerprint@2.1-service 
+#    android.hardware.biometrics.fingerprint@2.0-service 
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service_32 \
+    fingerprint.msm8937
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 # NFC
 PRODUCT_COPY_FILES += \
